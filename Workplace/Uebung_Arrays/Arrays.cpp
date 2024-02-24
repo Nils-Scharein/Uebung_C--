959361a3f6719c7d0d3d51e7c6268ea73d7530b3
Nils <nilsscharein@yahoo.de>
2024-02-24 16:44:59 +0100

    Update

--- Workplace/Uebung_Arrays/Arrays.cpp
+++ Workplace/Uebung_Arrays/Arrays.cpp
@@ -2,7 +2,8 @@
 
 int applyall(int arr1, size_t size1, int arr2, size_t size2)
 {
-    return 0;
+    int *arr_all[size1 * size2];
+    return arr_all;
 }
 
 int print_all(int arr_all, size_t size)
@@ -19,7 +20,7 @@ size_t size_array1 = sizeof(array1) / sizeof(array1[0]);
 int array2[] = {10, 20, 30};
 size_t size_array2 = sizeof(array2) / sizeof(array1[0]);
 
-std::cout << array1[0];
+std::cout << (array1, size_array1, array2 size_array2)
 
 
 return 0;
