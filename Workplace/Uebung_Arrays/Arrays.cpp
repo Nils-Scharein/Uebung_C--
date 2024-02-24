c51b5a2038d745b928913dc5bd4c0388dd59af4d
Nils <nilsscharein@yahoo.de>
2024-02-24 16:05:00 +0100

    Debuuger + Inhalte

---
+++ Workplace/Uebung_Arrays/Arrays.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int applyall(int arr1, size_t size1, int arr2, size_t size2)
+{
+    return 0;
+}
+
+int print_all(int arr_all, size_t size)
+{
+return 0;
+
+}
+
+
+int main()
+{
+int array1[] = {1, 2, 3, 4, 5};
+size_t size_array1 = sizeof(array1) / sizeof(array1[0]);
+int array2[] = {10, 20, 30};
+size_t size_array2 = sizeof(array2) / sizeof(array1[0]);
+
+std::cout << array1[0];
+
+
+return 0;
+}
--- Workplace/Uebung_Memory_Allocation/Memory.cpp
+++ Workplace/Uebung_Memory_Allocation/Memory.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+void reverse_array(int* arr, int size)
+{
+    int *end = (arr+size-1);
+    while (arr < end)
+    {
+        int temp = *arr; 
+        *arr = *end; 
+        *end = temp; 
+        arr++; 
+        end--; 
+    }
+}
+
+
 main()
 {
 
@@ -21,5 +35,9 @@ std::cout << temp_ptr << std::endl;
 
 return 0;
 
+int testarr[5] = {1, 6, 2, 5, 6};
+int sizearr = sizeof(testarr) / sizeof(testarr[0]);
+
+reverse_array(testarr, sizearr)
 
 }
