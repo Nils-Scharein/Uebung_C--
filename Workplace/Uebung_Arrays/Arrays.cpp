a7d986d62687710d38aa0397d32f1638a0a28260
Nils <nilsscharein@yahoo.de>
2024-02-26 18:14:00 +0100

    Fix Array Datei

--- Workplace/Uebung_Arrays/Arrays.cpp
+++ Workplace/Uebung_Arrays/Arrays.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 
-int* applyall(int arr1, size_t size1, int arr2, size_t size2)
+int *applyall(const int *const arr1, size_t size1, const int *const arr2, size_t size2)
 {
-    int *arr_all[size1 * size2];
-    for (int i = 0; i < sizeof(*arr_all) / sizeof(*arr_all[0]); i++)
-    {
-        std::coout 
+    int arr_all[size1 * size2];
+    int size_arr_all = sizeof(arr_all) / sizeof(arr_all[0]);
 
+    int position = 0;
+
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+            {
+                arr_all[k] = arr1[i] * arr2[j];
+                position++;
+            }
     }
 
-
-    return *arr_all;
+    return arr_all;
 }
 
-int print_all(int arr_all, size_t size)
+int print_all(int *const arr, size_t size)
 {
-return 0;
-
+    for(int l = 0; l < size; l++)
+    {
+       std::cout << arr[l] << std::endl;
+    }
+    return 0;
 }
-
 
 int main()
 {
-int array1[] = {1, 2, 3, 4, 5};
-size_t size_array1 = sizeof(array1) / sizeof(array1[0]);
-int array2[] = {10, 20, 30};
-size_t size_array2 = sizeof(array2) / sizeof(array1[0]);
+    int array1[] = {1, 2, 3, 4, 5};
+    size_t size_array1 = sizeof(array1) / sizeof(array1[0]);
+    int array2[] = {10, 20, 30};
+    size_t size_array2 = sizeof(array2) / sizeof(array1[0]);
 
-std::cout << (array1, size_array1, array2, size_array2);
+    int *ptr_all = applyall(array1, size_array1, array2, size_array2);
 
-
-return 0;
+    return 0;
 }
